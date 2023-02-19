36a6bf90a0afdaa0ce9f56b2eea87712af81a5e7
domieee <99103235+domieee@users.noreply.github.com>
2023-02-19 18:43:36 +0100

    added navigator.geolocation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,56 @@
-const urlConstructor = () => {
+/* 
+TODOS:
+    ! Figure out which weather conditions exist. Include function for different icons in case of different conditions.
+    ? Maybe include a function that keeps the site on load till the location track as either accepted or declined.
+*/
+
+const urlConstructor = (lat, lon) => {
     const city = document.querySelector('#city').value
     const apiKey = '584ebb9b612546f89b1185600231802'
     const url = new URL('http://api.weatherapi.com/v1/current.json')
     const urlParam = new URLSearchParams()
 
+    // ? Works, but maybe replace with switch for consistency
+    if (lat, lon) {
+        console.log(url);
+        urlParam.append('q', lat + ',' + lon)
+    } else if (city) {
+        urlParam.append('q', city)
+    }
+
     urlParam.append('key', apiKey)
-    urlParam.append('q', city)
     url.search = urlParam.toString()
+    console.log(url.toString());
 
     return url;
 }
 
+// Checks if either celsius, fahrenheit or both is selected. Returns the corresponding value.
 const celsiusFahrenheit = (data) => {
     const tempChoose = document.querySelector('#temp-choose').value
-    switch (tempChoose) {
+    switch (tempChoose) { 
         case 'c': return `${data.current.temp_c}°C` 
         case 'f': return `${data.current.temp_f}°F`
+        case 'cf': return `${data.current.temp_c}°C / ${data.current.temp_f}°F`
     }
 }
 
-function fetchData() {
-    fetch(urlConstructor())
+function fetchData(lat, lon) {
+    fetch(urlConstructor(lat, lon))
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            document.querySelector('#weather-icon').src = 'https://basmilius.github.io/weather-icons/production/fill/all/drizzle.svg'
             document.querySelector('#location').innerHTML = `${data.location.name}, ${data.location.country}`
             document.querySelector('#temperature-output').innerHTML = celsiusFahrenheit(data)
-            console.log(data.current.temp_c);
         });
-
 }
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        fetchData(position.coords.latitude, position.coords.longitude);
+      });
+}
+
+document.addEventListener('DOMContentLoaded', getLocation)
 
 document.querySelector('#search').addEventListener('click', fetchData, false);
